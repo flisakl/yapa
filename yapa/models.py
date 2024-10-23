ca19bd17b70190cf4066c527c71a4ad8aed6bbe6
Łukasz Flisak <lu.dev00@gmail.com>
2024-10-23 12:44:56 +0200

    Add basic User model

--- yapa/models.py
+++ yapa/models.py
@@ -1,3 +1,60 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.hashers import make_password
+from django.conf import settings
+import jwt
 
-# Create your models here.
+
+def generate_token(first_name, last_name, email):
+    return jwt.encode({
+            "first_name": first_name,
+            "last_name": last_name,
+            "email": email
+    }, settings.SECRET_KEY)
+
+
+class UserManager(BaseUserManager):
+    def _create_user_object(self, first_name, last_name, email, password):
+        if not first_name:
+            raise ValueError('First name must be provided')
+        if not last_name:
+            raise ValueError('Last name must be provided')
+        email = self.normalize_email(email)
+
+        user = self.model(first_name=first_name, last_name=last_name, email=email)
+        user.token = generate_token(first_name, last_name, email)
+        user.password = make_password(password)
+        return user
+
+    def create_user(self, first_name, last_name, email, password):
+        """
+        Create and save a user with the given first and last name, email and password.
+        """
+        user = self._create_user_object(first_name, last_name, email, password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, first_name, last_name, email, password):
+        """
+        Create and save a user with the given first and last name, email and password.
+        """
+        user = self._create_user_object(first_name, last_name, email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractUser):
+    username = None
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+    first_name = models.CharField(_("first name"), max_length=150, blank=False, null=False)
+    last_name = models.CharField(_("last name"), max_length=150, blank=False, null=False)
+    email = models.EmailField(unique=True)
+    token = models.CharField(max_length=1000)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
+
+    objects = UserManager()
