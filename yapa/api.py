62945ddba521ec0918c5a1f428e3fb129a93984d
Łukasz Flisak <lu.dev00@gmail.com>
2024-10-29 15:59:36 +0100

    Add basic asynchronous authentication

--- yapa/api.py
+++ yapa/api.py
@@ -3,8 +3,32 @@ from ninja.errors import ValidationError, _default_validation_error
 from django.urls import reverse_lazy
 
 from .users import router as auth_router
+from .models import User
 
-api = NinjaAPI()
+
+async def jwt_auth(request):
+    header = request.headers.get('Authorization')
+    if header:
+        _, token = header.split(' ')
+        try:
+            user = await User.objects.aget(token=token)
+            return user
+        except User.DoesNotExist:
+            pass
+
+
+async def jwt_auth_staff(request):
+    user = await jwt_auth(request)
+    if user and (user.is_staff or user.is_superuser):
+        return user
+
+
+async def jwt_auth_superuser(request):
+    user = await jwt_auth(request)
+    if user and user.is_superuser:
+        return user
+
+api = NinjaAPI(auth=jwt_auth)
 
 
 @api.exception_handler(ValidationError)
