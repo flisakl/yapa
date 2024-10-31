cc63a51864f0608f4a5c18eb0931e4c1eb551987
Łukasz Flisak <lu.dev00@gmail.com>
2024-10-31 11:43:25 +0100

    Add basic model for Task

--- yapa/models.py
+++ yapa/models.py
@@ -2,7 +2,9 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.hashers import make_password
+from django.core.validators import MinValueValidator
 from django.conf import settings
+from django.utils import timezone
 import jwt
 
 
@@ -76,3 +78,36 @@ class User(AbstractUser):
     REQUIRED_FIELDS = ["first_name", "last_name"]
 
     objects = UserManager()
+
+
+def yesterday():
+    date = timezone.now() - timezone.timedelta(days=1)
+    return date.replace(hour=23, minute=59, second=59)
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(blank=True, null=True, max_length=500)
+
+    created_at = models.DateTimeField(default=timezone.now)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='createdby', null=True)
+    completed_at = models.DateTimeField(blank=True, null=True, validators=[MinValueValidator(yesterday)])
+    completed_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='completedby')
+
+    class StatusEnum(models.IntegerChoices):
+        TODO = 0
+        IN_PROGRESS = 1
+        COMPLETED = 2
+
+    status = models.IntegerField(choices=StatusEnum, default=StatusEnum.TODO.value)
+
+    class PriorityEnum(models.IntegerChoices):
+        LOW = 0
+        MEDIUM = 1
+        HIGH = 2
+        CRITICAL = 3
+
+    priority = models.IntegerField(choices=PriorityEnum, default=PriorityEnum.LOW.value)
+
+    def __str__(self):
+        return self.name
